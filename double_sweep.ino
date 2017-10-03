3dc92dde1cd6050ac3f42017dca24cf8d67de5de
vector234 <32480956+vector234@users.noreply.github.com>
2017-10-03 18:38:47 +0800

    Add files via upload
    This code controls two servomotors via analog pins in order to circumvent the obstruction of the digital pins via a LCD shield that was in use. The servomotors are coded to sweep between two fixed angles to avoid contacting the supporting structure

---
+++ double_sweep.ino
@@ -0,0 +1,38 @@
+/* Sweep
+ by BARRAGAN <http://barraganstudio.com> 
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Sweep
+*/ 
+
+#include <Servo.h> 
+ 
+Servo myservo;  // create servo object to control a servo 
+                // twelve servo objects can be created on most boards
+Servo myservo2;
+int pos = 0;    // variable to store the servo position 
+ 
+void setup() 
+{ 
+  myservo2.attach(22);
+  myservo.attach(26);  // attaches the servo on pin 9 to the servo object 
+} 
+ 
+void loop() 
+{ 
+  for(pos = 65; pos <= 125; pos += 1) // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    myservo2.write(pos);
+    delay(30);                       // waits 15ms for the servo to reach the position 
+  } 
+  for(pos = 125; pos>=65; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    myservo.write(pos);              // tell servo to go to position in variable 'pos' 
+    myservo2.write(pos);
+    delay(30);                       // waits 15ms for the servo to reach the position 
+  } 
+} 
+
